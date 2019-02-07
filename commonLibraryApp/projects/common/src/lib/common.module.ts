b818f268003ed80336063cf47e36aa9913e8b60c
Adam Coard <arcoard+github@gmail.com>
2019-02-07 10:52:35 -0500

    Ran ng g lib, created new library with defaults.

---
+++ commonLibraryApp/projects/common/src/lib/common.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonComponent } from './common.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [CommonComponent],
+  exports: [CommonComponent]
+})
+export class CommonModule { }
