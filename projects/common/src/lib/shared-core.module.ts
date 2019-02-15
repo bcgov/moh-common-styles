4b95347867170c9ee54f49a0314659f475f8dcea
Adam Coard <arcoard+github@gmail.com>
2019-02-15 13:21:01 -0500

    Major breakthrough as importing variables.scss and bootstrap variables is working in wizardprogressbar

--- projects/common/src/lib/shared-core.module.ts
+++ projects/common/src/lib/shared-core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { CoreBreadcrumbComponent } from './components/core-breadcrumb/core-breadcrumb.component';
+import { CoreBreadcrumbComponent } from './components/core-breadcrumb/core-breadcrumb.component';
 // import { FormActionBarComponent } from './components/form-action-bar/form-action-bar.component';
 // import { PageFrameworkComponent } from './components/page-framework/page-framework.component';
 import { PasswordComponent } from './components/password/password.component';
-// import { WizardProgressBarComponent } from './components/wizard-progress-bar/wizard-progress-bar.component';
+import { WizardProgressBarComponent } from './components/wizard-progress-bar/wizard-progress-bar.component';
 import { NgForm, FormsModule } from '@angular/forms';
 import { ProgressbarModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
@@ -21,22 +21,22 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   declarations: [
-    // CoreBreadcrumbComponent,
+    CoreBreadcrumbComponent,
     // FormActionBarComponent,
     // PageFrameworkComponent,
     PasswordComponent,
-    // WizardProgressBarComponent,
+    WizardProgressBarComponent,
     // DateComponent,
     // DayValidationDirective,
     // DateFieldFormatDirective,
     // YearValidateDirective
   ],
   exports: [
-    // CoreBreadcrumbComponent,
+    CoreBreadcrumbComponent,
     // FormActionBarComponent,
     // PageFrameworkComponent,
     PasswordComponent,
-    // WizardProgressBarComponent,
+    WizardProgressBarComponent,
     // DateComponent,
     // DayValidationDirective,
     // DateFieldFormatDirective,
