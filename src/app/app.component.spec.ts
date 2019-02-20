48e6e853e42a4eaca6b3fd273c065bfe55b8220f
Adam Coard <arcoard+github@gmail.com>
2019-02-20 14:29:20 -0500

    Have library-app test passing after changing @types/jasmine version.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to commonLibraryApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
