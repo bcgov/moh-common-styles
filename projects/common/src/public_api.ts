632e1713406dc0349d22e28b10ff418aa7836949
Adam Coard <arcoard+github@gmail.com>
2019-02-20 15:08:52 -0500

    Updated package exports

--- projects/common/src/public_api.ts
+++ projects/common/src/public_api.ts
@@ -2,17 +2,18 @@
  * Public API Surface of moh-common-styles
  */
 
-export * from './lib/common.service';
 export * from './lib/shared-core.module';
 
-
 // Models
-export * from './lib/models/container';
+export { Container } from './lib/models/container';
 export { AbstractForm } from './lib/models/abstract-form';
 export { Address } from './lib/models/address.model';
-export * from './lib/models/base';
+export { Base } from './lib/models/base';
 export { Person } from './lib/models/person.model';
 
+// Services
+export { GeocoderService } from './lib/services/geocoder.service';
+export { AbstractHttpService } from './lib/services/abstract-api-service';
 
 // Interfaces
-export * from './lib/interfaces/simple-date.interface';
+export { SimpleDate } from './lib/interfaces/simple-date.interface';
