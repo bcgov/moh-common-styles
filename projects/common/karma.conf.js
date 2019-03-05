09859942dd4e8f399cd38f75e17cc6e7323f42f7
Adam Coard <arcoard+github@gmail.com>
2019-03-05 16:12:10 -0500

    Switched to Chrome Headless for unit-tests

--- projects/common/karma.conf.js
+++ projects/common/karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    // browsers: ['Chromeheadless'], // todo - setup chromeheadless
+    // browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'], // todo - setup chromeheadless
     singleRun: false
   });
 };
