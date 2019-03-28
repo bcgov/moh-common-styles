35d0bba6fe35c293d43161790a6d2c075c4d46b3
Adam Coard <arcoard+github@gmail.com>
2019-03-28 12:53:46 -0400

    feat(captcha): CaptchaModule properly configured so docs are now correct.

--- projects/common/captcha/src/captcha.module.ts
+++ projects/common/captcha/src/captcha.module.ts
@@ -1,29 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-// import { AppComponent } from './app.component';
 import { CaptchaComponent } from './captcha.component';
 import { CaptchaDataService } from './captcha-data.service';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    // AppComponent,
     CaptchaComponent
   ],
   imports: [
-    BrowserModule,
     HttpClientModule,
+    CommonModule,
     FormsModule
   ],
   providers: [
     CaptchaDataService
   ],
-
   exports: [
     CaptchaComponent
   ],
-  // bootstrap: [AppComponent]
 })
-export class CaptchaModule { }
+export class CaptchaModule { 
+  static forRoot(): ModuleWithProviders<CaptchaModule> {
+    return {
+      ngModule: CaptchaModule,
+      providers: [CaptchaDataService]
+    }
+  }
+}
