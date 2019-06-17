59f20b4afbee2fc9a3af33aeb162a342e5a5f415
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-06-17 13:29:24 -0400

    WIP: Remove i18n folder, added bc postal code validator

---
+++ projects/common/lib/components/postal-code/validate-bc-postal.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ValidateBcPostalDirective } from './validate-bc-postal.directive';
+
+describe('ValidateBcPostalDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ValidateBcPostalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
