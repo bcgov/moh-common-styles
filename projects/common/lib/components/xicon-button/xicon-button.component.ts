baee5ad8949a9b9e97d56864fed60aca669611da
Adam Coard <arcoard+github@gmail.com>
2019-07-04 18:25:52 -0400

    refactor(XIconButton): Add (click) event, @deprecate (clickEvent)

--- projects/common/lib/components/xicon-button/xicon-button.component.ts
+++ projects/common/lib/components/xicon-button/xicon-button.component.ts
@@ -8,11 +8,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class XiconButtonComponent {
 
   @Input() label: string;
+  /**
+   * @deprecated - Remove at breaking change and go to (click)
+   */
   @Output() clickEvent: EventEmitter<any> = new EventEmitter<any>();
+  @Output() click: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {}
 
   onBtnClick() {
     this.clickEvent.emit();
+    this.click.emit();
   }
 }
