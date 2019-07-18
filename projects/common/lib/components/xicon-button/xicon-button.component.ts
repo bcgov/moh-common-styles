867f969d3936abc45586f5e875120902608943b4
Adam Coard <arcoard+github@gmail.com>
2019-07-18 11:50:45 -0400

    a11y(XIconButton): Add label

--- projects/common/lib/components/xicon-button/xicon-button.component.ts
+++ projects/common/lib/components/xicon-button/xicon-button.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { MoHCommonLibraryError } from '../../../helpers/library-errorr';
 
 @Component({
   selector: 'common-xicon-button',
   templateUrl: './xicon-button.component.html',
   styleUrls: ['./xicon-button.component.scss']
 })
-export class XiconButtonComponent {
+export class XiconButtonComponent implements OnInit {
 
+  /**
+   * Label to use for accessibility.
+   * @required
+   */
   @Input() label: string;
   /**
    * @deprecated - Remove at breaking change and go to (click)
@@ -14,7 +19,18 @@ export class XiconButtonComponent {
   @Output() clickEvent: EventEmitter<any> = new EventEmitter<any>();
   @Output() click: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit() {
+    if (!this.label) {
+      // todo - change to MohCommmonLibraryError -> test in IE10
+      // throw Error('common-xicon-button initialized without label. You MUST supply a label attribute for accessibility.');
+      const msg = `common-xicon-button initialized without label. You MUST supply a label attribute for accessibility.
+      e.g. <common-xicon-button label='Remove Spouse'>
+`; // Intentional to create a blank line between our error and stack trace.
+      throw new MoHCommonLibraryError(msg);
+    }
+  }
 
   onBtnClick() {
     this.clickEvent.emit();
