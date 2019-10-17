a27ad942613f61692e92fe4ed9c4f71414c5b162
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-10-17 17:25:03 -0400

    WIP: deleted spec file for abstract form control

--- projects/common/lib/models/abstract-form-cntrl.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractFormCntrl } from './abstract-form-cntrl';
-
-describe('AbstractFormCntrl', () => {
-  it('should create an instance', () => {
-    expect(new AbstractFormCntrl()).toBeTruthy();
-  });
-});
