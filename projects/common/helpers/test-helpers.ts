8248e61f49c2218b05ae03b71b5661c716b21b0f
Adam Coard <arcoard+github@gmail.com>
2019-11-05 11:20:25 -0500

    test: Remove extra unnecessary tick() call.

--- projects/common/helpers/test-helpers.ts
+++ projects/common/helpers/test-helpers.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, tick } from '@angular/core/testing';
 
 export function tickAndDetectChanges(fixture: ComponentFixture<any>) {
-  tick(100);
   fixture.detectChanges();
   tick();
 }
