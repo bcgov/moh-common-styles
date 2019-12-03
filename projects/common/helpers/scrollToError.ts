553adc1d8a0440298292da6d752df5ab7f96d84d
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-12-02 22:05:47 -0500

    fix: Scrolling to be compatible with IE - IE does not support ScrollToOptions.

--- projects/common/helpers/scrollToError.ts
+++ projects/common/helpers/scrollToError.ts
@@ -1,10 +1,15 @@
+
 /**
  * Get the first visible common-error-container and smoothly scroll to it.
  */
 export function scrollToError(yOffset = -75) {
     const el = document.querySelector('common-error-container .text-danger');
     if (el) {
-        const top = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
-        window.scrollTo({top, behavior: 'smooth'});
+     const top = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+     /**
+      * IE does not support Window.scrollTo({ScrollToOptions}),
+      * For compatibility modied to use method without ScrollToOptions Objec.
+      */
+      window.scrollTo(0, top);
     }
 }
