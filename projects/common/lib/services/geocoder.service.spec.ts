ab4688c508dcae931d940b7aa7e932726a246607
Harry Maynard <harry.maynard@maximusbc.ca>
2020-02-13 13:57:04 -0500

    feat: Added unit tests for GeocoderService.

---
+++ projects/common/lib/services/geocoder.service.spec.ts
@@ -0,0 +1,58 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+
+import { GeocoderService, GeoAddressResult } from './geocoder.service';
+
+describe('GeocoderService', () => {
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ GeocoderService ]
+    });
+  });
+
+  it('should be created', () => {
+    const service: GeocoderService = TestBed.get(GeocoderService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should replace \'--\' with \'-\' in the address.', (done) => {
+    let httpClientSpy: { get: jasmine.Spy };
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    let service = new GeocoderService(<any> httpClientSpy);
+    
+    const mockResponseAddress = {
+      fullAddress: 'Unit 101 -- 123 Streetname Rd., city, BC, CAN',
+      localityName: 'city',
+      country: 'CAN',
+      provinceCode: 'BC',
+    }
+    const mockPayloadResponse = {
+      features: [
+        {
+          properties: {
+            ...mockResponseAddress
+          }
+        }
+      ]
+    };
+    const expectedResult: GeoAddressResult[] = [
+      {
+        fullAddress: 'Unit 101 - 123 Streetname Rd., city, BC, CAN',
+        city: 'city',
+        street: 'Unit 101 - 123 Streetname Rd.',
+        country: 'CAN',
+        province: 'BC',
+      }
+    ];
+    httpClientSpy.get.and.returnValue(of(mockPayloadResponse));
+
+    service.lookup('Query Text').subscribe((addresses) => {
+      expect(addresses).toEqual(expectedResult);
+      done();
+    });
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+});
