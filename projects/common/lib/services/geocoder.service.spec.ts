c8107a7aeba4c34f936678e2c11350e95b71fc2d
Harry Maynard <harry.maynard@maximusbc.ca>
2020-02-14 12:44:06 -0500

    feat: Added another unit test for GeocoderService.

--- projects/common/lib/services/geocoder.service.spec.ts
+++ projects/common/lib/services/geocoder.service.spec.ts
@@ -4,6 +4,31 @@ import { of } from 'rxjs';
 
 import { GeocoderService, GeoAddressResult } from './geocoder.service';
 
+const mockResponseAddress = {
+  fullAddress: 'Unit 101 -- 123 Streetname Rd., city, BC, CAN',
+  localityName: 'city',
+  country: 'CAN',
+  provinceCode: 'BC',
+}
+const mockPayloadResponse = {
+  features: [
+    {
+      properties: {
+        ...mockResponseAddress
+      }
+    }
+  ]
+};
+const expectedResult: GeoAddressResult[] = [
+  {
+    fullAddress: 'Unit 101 - 123 Streetname Rd., city, BC, CAN',
+    city: 'city',
+    street: 'Unit 101 - 123 Streetname Rd.',
+    country: 'CAN',
+    province: 'BC',
+  }
+];
+
 describe('GeocoderService', () => {
   
   beforeEach(() => {
@@ -23,30 +48,6 @@ describe('GeocoderService', () => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     let service = new GeocoderService(<any> httpClientSpy);
     
-    const mockResponseAddress = {
-      fullAddress: 'Unit 101 -- 123 Streetname Rd., city, BC, CAN',
-      localityName: 'city',
-      country: 'CAN',
-      provinceCode: 'BC',
-    }
-    const mockPayloadResponse = {
-      features: [
-        {
-          properties: {
-            ...mockResponseAddress
-          }
-        }
-      ]
-    };
-    const expectedResult: GeoAddressResult[] = [
-      {
-        fullAddress: 'Unit 101 - 123 Streetname Rd., city, BC, CAN',
-        city: 'city',
-        street: 'Unit 101 - 123 Streetname Rd.',
-        country: 'CAN',
-        province: 'BC',
-      }
-    ];
     httpClientSpy.get.and.returnValue(of(mockPayloadResponse));
 
     service.lookup('Query Text').subscribe((addresses) => {
@@ -54,5 +55,15 @@ describe('GeocoderService', () => {
       done();
     });
     expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should replace \'--\' with \'-\' in the address in processResponse.', () => {
+    class GeocoderServiceTest extends GeocoderService {
+      public processResponseTest() {
+        return this.processResponse(mockPayloadResponse)
+      }
+    }
+    let serviceTest = new GeocoderServiceTest(<any> {});
+    expect(serviceTest.processResponseTest()).toEqual(expectedResult)
   });
 });
