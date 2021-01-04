e43d472a01bf70a42b1861157f2a352161606fd0
Bryce McMath <bryce.j.mcmath@gmail.com>
2021-01-04 11:43:51 -0800

    fix: fix a11y issues with province and country dropdowns

--- projects/common/lib/components/address/address.component.ts
+++ projects/common/lib/components/address/address.component.ts
@@ -173,6 +173,7 @@ export class AddressComponent extends Base
    * @param value
    */
   setCountry(value: string) {
+    console.log('setCountry was called with:', value);
     this.addr.province = this.setDefaultProvinceAsOption( value );
     this.addr.country = value;
     this.updateProvList();
@@ -250,8 +251,10 @@ export class AddressComponent extends Base
     if (changes['countryList'] && changes['countryList'].currentValue) {
 
       if (this.addr && !this.addr.country) {
+
         // Set defaults
         this.addr.country = this.setDefaultCountryAsOption();
+        console.log('there was no country assigned so a default was');
 
         // Set defaults
         this.addr.province = this.setDefaultProvinceAsOption(this.addr.country);
@@ -262,6 +265,8 @@ export class AddressComponent extends Base
       if (this.addr && !this.addr.province) {
         // Set defaults
         this.addr.province = this.setDefaultProvinceAsOption(this.addr.country);
+
+        console.log('there was no province assigned so a default was');
       }
       this.updateProvList();
     }
@@ -357,6 +362,7 @@ export class AddressComponent extends Base
 
   // Only BC addresses therefore no need to copy province into structure.
   setAddress(data: GeoAddressResult) {
+    console.log('setAddress, data: ', data);
     this.addr.addressLine1 = data.street;
     this.addr.city = data.city;
     this.addr.province = data.province;
--- projects/common/lib/components/province/province.component.ts
+++ projects/common/lib/components/province/province.component.ts
@@ -46,7 +46,7 @@ export class ProvinceComponent extends AbstractFormControl implements OnInit {
   @Input() placeholder: string = 'Please select a province';
   @Input() maxlength: string = '250';
   @Input() useDropDownList: boolean = true;
-
+  @Input() bcOnly: boolean = false;
   @Input()
   set value( val: string ) {
     if ( val ) {
@@ -80,17 +80,24 @@ export class ProvinceComponent extends AbstractFormControl implements OnInit {
     super.ngOnInit();
   }
 
-  onValueChange( value: any ) {
-    if ( value !== this.province ) {
-      this._onChange( value );
-      this.valueChange.emit( value );
-      this.province = value;
+  provinceChange( event: any ) {
+    const province = this.provinceList[Number(event.target.value)];
+    console.log('province:', province);
+    if (province) {
+      console.log('provinceCode:', province.provinceCode);
+      this._onChange(province.provinceCode);
+      this.valueChange.emit(province.provinceCode);
+      this.province = province.provinceCode;
     }
   }
 
-  onBlur( event: any ) {
-    this._onTouched( event );
-    this.blur.emit( event );
+  typedProvinceChange(event: any) {
+    const province = event.target.value;
+    if ( province ) {
+      this._onChange(province);
+      this.valueChange.emit(province);
+      this.province = province;
+    }
   }
 
   writeValue( value: any ): void {
